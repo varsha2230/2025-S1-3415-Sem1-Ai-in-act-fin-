fe7450a82f2a3924cb5efabf31472d3c8b7fc41f
varsha2230 <N2504228L@e.ntu.edu.sg>
2025-08-19 02:48:47 +0000

    1

---
+++ app.py
@@ -0,0 +1,13 @@
+print('starting flask')
+
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run(port=1234)
+    

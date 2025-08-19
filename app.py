53a3fab661a34f1baa9af6077777f79312e8200f
varsha2230 <N2504228L@e.ntu.edu.sg>
2025-08-19 03:47:38 +0000

    1

--- app.py
+++ app.py
@@ -3,11 +3,17 @@ print('starting flask')
 from flask import Flask,render_template,request
 
 app = Flask(__name__)
-
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q= float(request.form.get('q'))
+    return(render_template("dbs.html", r=(-50.6*q)+90.2))
 if __name__ == "__main__":
     app.run(port=1234)
-    
+
